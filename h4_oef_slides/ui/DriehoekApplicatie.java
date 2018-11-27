81ce596a359d1d3c4e009f751967d3ff83d4f479
biebem <lammens.f96@gmail.com>
2018-11-27 16:18:58 +0100

    upload Driehoek

---
+++ h4_oef_slides/ui/DriehoekApplicatie.java
@@ -0,0 +1,26 @@
+package ui;
+
+import domein.Driehoek;
+import java.util.Scanner;
+
+public class DriehoekApplicatie {
+
+    public static void main(String[] args) {
+        int zijde1;
+        int zijde2;
+        int zijde3;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Geef een geheel getal verschillend van 0 voor de eerste zijde : ");
+        zijde1 = sc.nextInt();
+        System.out.print("Geef een geheel getal verschillend van 0 voor de tweede zijde : ");
+        zijde2 = sc.nextInt();
+        System.out.print("Geef een geheel getal verschillend van 0 voor de derde zijde : ");
+        zijde3 = sc.nextInt();
+        Driehoek driehoek1 = new Driehoek(zijde1,zijde2,zijde3);
+        if (driehoek1.isRechtHoekigeDriehoek() == true)
+            System.out.printf("De driehoek met zijden %d , %d , %d is rechthoekig%n",zijde1,zijde2,zijde3);
+        else 
+            System.out.printf("De driehoek met zijden %d , %d , %d is niet rechthoekig%n",zijde1,zijde2,zijde3);
+    }
+    
+}
